51e1cb3619e394ff6f013c36666839070c923249
Fernando <fernando@Raffaelles-MBP.fritz.box>
2018-04-24 17:58:15 +0200

    Create the initial home view

--- EventApp/HomeController.swift
+++ EventApp/HomeController.swift
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView?.backgroundColor = .gray
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
